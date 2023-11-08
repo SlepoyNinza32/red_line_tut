b01a92fd2d09b00f87f97530da560c5635476331
slwpoyninza32@gmail.com <slwpoyninza32@gmail.com>
2023-11-08 15:32:32 +0500

    jj

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:red_line_tut/my_app.dart';
+import 'FirebaseMethods.dart';
+import 'firebase_options.dart';
 import 'model/profile_model.dart';
 import 'pages/pages.dart';
 
 void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter<ProfileModel>(ProfileModelAdapter());
@@ -18,4 +25,12 @@ void main() async {
       home: have == true ? MyApp() : FirstPage(),
     ),
   );
+  FirebaseFirestore.instance
+      .collection('users')
+      .get()
+      .then((QuerySnapshot querySnapshot) {
+    querySnapshot.docs.forEach((doc) {
+      print(doc.id);
+    });
+  });
 }
